e1b9af100f5c2c876a49f034402392be57695d8f
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-02-07 09:36:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar, Text, View } from "react-native";
 export default function App() {
   return (
     <>
-      <StatusBar barStyle={"dark-content"} />
+      <StatusBar />
       <View>
         <Text>Seres Humaaaaaaaaaanos!</Text>
       </View>
