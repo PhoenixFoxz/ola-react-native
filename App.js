c34a04db14576e21d01a55d4106d634be599de34
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-02-07 10:30:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const vai = () => {
+    Alert.alert("Titulo da janela", "Fala parça!");
+  };
+
   return (
     <>
       <StatusBar />
-      <View>
-        <Text>Seres Humaaaaaaaaaanos!</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Topo/Cabeçalho</Text>
+        </View>
+        <View>
+          <Text>Conteúdo...</Text>
+        </View>
+        <View>
+          <Text>Rodapé...</Text>
+          <Button title="Toca aqui!!!" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
